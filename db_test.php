56f09276ea2c60eb0bcbae7c66f029c82615ed59
testktestt <testk.testt@gmail.com>
2015-12-31 22:01:52 +0900

    add: mysqli_set_chareset()

--- db_test.php
+++ db_test.php
@@ -1,6 +1,7 @@
 <!--?php <br ?--> $link = mysqli_connect('localhost','railsuser','mypassword','sample_app_development');
   if (!$link) {
-    die('connect error12356789!!!!!!!'.mysql_error());
+    die('connect error'.mysql_error());
   } else {
-    echo 'connect sucsess!!!!!!!';
+    echo 'connect sucsess';
+    mysqli_set_charset($link, "utf8");
   }&gt;
