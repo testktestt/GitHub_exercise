753d94334a0a242420e56edb46da8f1db622e876
kei <kei@milkt.com>
2015-12-31 09:18:46 +0900

    2nd commit 2

--- db_test.php
+++ db_test.php
@@ -1,6 +1,6 @@
 <!--?php <br ?--> $link = mysqli_connect('localhost','railsuser','mypassword','sample_app_development');
   if (!$link) {
-    die('connect error123!!!!!!!'.mysql_error());
+    die('connect error12356789!!!!!!!'.mysql_error());
   } else {
-    echo 'connect sucsess123!!!!!!!';
+    echo 'connect sucsess!!!!!!!';
   }&gt;
